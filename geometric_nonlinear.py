68fa676e85dffbc9a92ce8af4949a538888b5694
mohanxuan <1175448684@qq.com>
2022-09-08 02:28:09 +0800

    initial state 01: maximum dof is printed

--- geometric_nonlinear.py
+++ geometric_nonlinear.py
@@ -5,6 +5,7 @@ from stiffnessMtrx import System_of_equations
 from readInp import *
 from material import *
 from body import Body
+from tiMath import field_abs_max
 
 
 if __name__ == "__main__":
@@ -53,4 +54,5 @@ if __name__ == "__main__":
     print("\033[35;1m maximum stress[{}] = {} MPa \033[0m".format(stress_id, abs(stress).max()), end="; ")
     print("\033[35;1m maximum cauchy_stress[{}] = {} MPa \033[0m".format(
         stress_id, abs(equationSystem.body.cauchy_stress.to_numpy()[:, :, stress_id]).max()))
+    print("\033[40;33;1m max dof (disp) = {} \033[0m".format(field_abs_max(equationSystem.dof)))
     equationSystem.body.show2d(disp=equationSystem.dof, field=stress)
